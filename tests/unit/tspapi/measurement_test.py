6e28751e1042e1a4448511838ab770f927a0afcc
David Gwartney <davidg@boundary.com>
2016-08-15 17:42:06 -0700

    Add timezone so tests pass regarding what time zone the operating system is running in

--- tests/unit/tspapi/measurement_test.py
+++ tests/unit/tspapi/measurement_test.py
@@ -132,25 +132,25 @@ class MeasurementTest(TestCase):
             value += 0.1
 
     def test_parse_timestamp_date_string_yymmddhhmm(self):
-        s = '2016-01-27 3:38AM'
+        s = '2016-01-27 3:38AM PST'
         d = Measurement.parse_timestamp(s)
         self.assertEqual(type(d), int)
         self.assertEqual(d, 1453894680)
 
     def test_parse_timestamp_date_string_yymmddhhmmss(self):
-        s = '2016-01-27 3:38:25AM'
+        s = '2016-01-27 3:38:25AM PST'
         d = Measurement.parse_timestamp(s)
         self.assertEqual(type(d), int)
         self.assertEqual(d, 1453894705)
 
     def test_parse_timestamp_date_string_yymmddHHMM(self):
-        s = '2003-08-16 20:06:01'
+        s = '2003-08-16 20:06:01 PST'
         d = Measurement.parse_timestamp(s)
         self.assertEqual(type(d), int)
         self.assertEqual(d, 1061089561)
 
     def test_parse_timestamp_date_string_yymmddHHMMSS(self):
-        s = '2001-03-27 19:07:32'
+        s = '2001-03-27 19:07:32 PST'
         d = Measurement.parse_timestamp(s)
         self.assertEqual(type(d), int)
         self.assertEqual(d, 985748852)
